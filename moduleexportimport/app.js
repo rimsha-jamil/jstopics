ea8301397a165d0f5f4b0be35a380b229ef9c864
rimsha-jamil <rimshadev4@gmail.com>
2025-07-28 16:58:24 +0500

    init

---
+++ moduleexportimport/app.js
@@ -0,0 +1,5 @@
+// app.js
+import { add, multiply } from './math.js';
+
+console.log("2 + 3 =", add(2, 3));         // Output: 2 + 3 = 5
+console.log("2 x 3 =", multiply(2, 3));    // Output: 2 x 3 = 6
---
+++ moduleexportimport/math.js
@@ -0,0 +1,3 @@
+// math.js
+export const add = (a, b) => a + b;
+export const multiply = (a, b) => a * b;
